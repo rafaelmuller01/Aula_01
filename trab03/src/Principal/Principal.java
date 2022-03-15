b79437c4cf6fda34684d8e72a13377e4ca8ffb6f
Rafael muller <83794212+rafaelmuller01@users.noreply.github.com>
2022-03-14 21:48:39 -0300

    Add files via upload

--- trab03/src/Principal/Principal.java
+++ trab03/src/Principal/Principal.java
@@ -1,51 +1,16 @@
 package Principal;
-import java.util.*;
+
+import Conta.Conta;
+import Cliente.Cliente;
+
 
 public class Principal {
 	public static void main(String[] args) {
 		
-		String cpf;
-		int anoNascimento;
-		String nome;
-		String email;
 		
-		Scanner ler = new Scanner(System.in);
+		Cliente cliente01 = new Cliente("03510400902", 1985, "ana clara", "aninha@gmail.com");
 		
-		System.out.println("Digite seu CPF: ");
-		cpf = ler.nextLine();
+		Conta conta01 = new Conta(1519, 750.95f);
 		
-		System.out.println("Digite seu ano de nascimento: ");
-		anoNascimento = ler.nextInt();
-		ler.nextLine();
-		
-		System.out.println("Digite seu nome: ");
-		nome = ler.nextLine();
-		
-		System.out.println("Digite seu email: ");
-		email = ler.nextLine();
-		
-		Cliente cliente01 = new Cliente(cpf, anoNascimento,nome, email);
-		/* erro import */
-		
-		System.out.println(cliente01.retornaCliente());
-		
-		float numero;
-		float saldo;
-		
-		System.out.println("Digite o numero da sua conta:");
-		numero = ler.nextFloat();
-		
-		System.out.println("Digite o seu saldo:");
-		saldo = ler.nextFloat();
-		
-		Cliente conta1 = new Cliente(cpf, anoNascimento, nome, email);
-		
-		Conta conta01 = new Conta(numero, saldo);
-		/* erro import */
-		
-		System.out.println(conta01.retornaConta());
-		
-		ler.close();
 	}
 }
-/* se passar para o mesmo pacote que a principal ele para de dar o erro de import*/
